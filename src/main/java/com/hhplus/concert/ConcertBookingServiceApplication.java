c80def491ace2ed776f80a649ac6967c0393cb71
simhw <simhw96@gmail.com>
2024-10-11 02:50:08 +0900

    init

---
+++ src/main/java/com/hhplus/concert/ConcertBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.concert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertBookingServiceApplication.class, args);
+	}
+
+}
