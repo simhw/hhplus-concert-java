3c94a495513d3f98333310e60a4b33e5a713fea7
simhw <simhw96@gmail.com>
2024-11-07 19:10:16 +0900

    feat: global cache(redis) 적용

--- src/main/java/com/hhplus/concert/application/ConcertFacade.java
+++ src/main/java/com/hhplus/concert/application/ConcertFacade.java
@@ -2,6 +2,7 @@ package com.hhplus.concert.application;
 
 import com.hhplus.concert.domain.concert.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public class ConcertFacade {
     /**
      * 삭제되지 않은 콘서트 목록 조회
      */
+    @Cacheable(value = "concerts", cacheNames = "concerts", key = "'all'")
     public List<ConcertInfo> getConcertInfos() {
         return concertService.getConcertInfos();
     }
@@ -22,6 +24,7 @@ public class ConcertFacade {
     /**
      * 예약 가능한 공연 목록 조회
      */
+    @Cacheable(value = "concert_performances", cacheNames = "concert_performances", key = "#concertId")
     public List<ConcertPerformanceInfo> getAvailablePerformanceInfos(Long concertId) {
         return concertService.getAvailablePerformanceInfos(concertId);
     }
---
+++ src/main/java/com/hhplus/concert/infra/config/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.hhplus.concert.infra.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@AllArgsConstructor
+public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory connectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
